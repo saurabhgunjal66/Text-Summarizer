2ec8962a7d2cfbace8c7fff33413bc720e6acd0f
saurabhgunjal66 <161634150+saurabhgunjal66@users.noreply.github.com>
2024-07-13 13:57:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,6 @@ if __name__=='__main__':
     app.run(debug=True)
 
 
-# from flask import Flask, render_template
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 
